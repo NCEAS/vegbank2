61d9cadf5893c81d5abd1630dd4e5542d34d2f89
RWShelton <rshelton@nceas.ucsb.edu>
2025-01-27 17:32:58 -0800

    Adding a get_all_plots method, document for port forwarding testing

--- src/api/vegbankapi.py
+++ src/api/vegbankapi.py
@@ -27,11 +27,33 @@ def get_plots(accessioncode):
             data = (accessioncode, )
             print("Querying: " + cur.mogrify(SQL, data))
             cur.execute(SQL, data)
+            columns = [desc[0] for desc in cur.description]
             for record in cur.fetchall():
-                toReturn.append(record)
+                toReturn.append(dict(zip(columns, record)))
                 print(record)
         conn.close()
     return jsonify(toReturn)
+
+@app.route("/plots")
+def get_all_plots():
+    print("getting all plots")
+    toReturn = []
+    params = {
+        'dbname' : 'vegbank',
+        'user' : 'vegbank',
+        'password' : 'PASS', #TODO: set up secret for db password. This is not the real one. 
+        'host' : 'vegbankdb-postgresql', #service name via bitnami postgres
+        'port' : '5432'
+    }
+    with psycopg.connect(**params, cursor_factory=ClientCursor) as conn:
+        with conn.cursor() as cur:
+            SQL = "SELECT * from plot;"
+            cur.execute(SQL)
+            for record in cur.fetchall():
+                toReturn.append(record)
+        conn.close()    
+    return jsonify(toReturn)
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80,debug=True)
