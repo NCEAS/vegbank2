7575168b2c0823a059404e8a38cbe7918241102e
RWShelton <rshelton@nceas.ucsb.edu>
2025-05-30 12:36:15 -0700

    Enumerating fields in party queries

--- queries/party/get_parties_full.sql
+++ queries/party/get_parties_full.sql
@@ -1,5 +1,12 @@
 SELECT 
-    * 
+    party_ID,
+    salutation, 
+    givenName, 
+    middleName,
+    surName, 
+    organizationName,
+    contactInstructions,
+    accessionCode as partyAccessionCode
 FROM
     party
 WHERE partypublic IS NOT false
