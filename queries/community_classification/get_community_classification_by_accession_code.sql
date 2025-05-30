a3b61fdbe9ef855f4e2177045602cf67d3b2e164
RWShelton <rshelton@nceas.ucsb.edu>
2025-05-30 16:41:39 -0700

    Enumerating the fields in several queries

--- queries/community_classification/get_community_classification_by_accession_code.sql
+++ queries/community_classification/get_community_classification_by_accession_code.sql
@@ -1,6 +1,25 @@
 SELECT 
-    commClass.*,
-    commInterpretation.*
+    commClass.accessionCode as commClassAccessionCode,
+    commClass.classStartDate,
+    commClass.classStopDate,
+    commClass.inspection,
+    commClass.tableAnalysis,
+    commClass.multivariateanalysis,
+    commClass.expertSystem,
+    commClass.classnotes,
+    commClass.commname,
+    commClass.commcode,
+    commClass.commframework,
+    commClass.commLevel,
+    commClass.emb_commclass,
+    commInterpretation.commconcept_ID,
+    commInterpretation.classfit,
+    commInterpretation.classConfidence,
+    commInterpretation.commauthority_id,
+    commInterpretation.notes,
+    commInterpretation.type,
+    commInterpretation.nomenclaturaltype,
+    commInterpretation.emb_comminterpretation
 FROM
     commClass
     LEFT JOIN commInterpretation on commClass.commClass_ID = commInterpretation.commClass_ID
--- queries/taxon_observation/get_top_taxa_coverage.sql
+++ queries/taxon_observation/get_top_taxa_coverage.sql
@@ -1,5 +1,22 @@
 SELECT 
-    t.*
+    t.accessioncode as taxonObservationAccessionCode, 
+    t.authorplantname, 
+    t.emb_taxonobservation, 
+    t.int_currplantcode, 
+    t.int_currplantcommon, 
+    t.int_currplantconcept_id, 
+    t.int_currplantscifull, 
+    t.int_currplantscinamenoauth, 
+    t.int_origplantcode, 
+    t.int_origplantcommon, 
+    t.int_origplantconcept_id, 
+    t.int_origplantscifull, 
+    t.int_origplantscinamenoauth, 
+    t.maxcover, 
+    t.observation_id, 
+    t.reference_id, 
+    t.taxoninferencearea, 
+    t.taxonobservation_id
 FROM
 observation
 CROSS JOIN LATERAL
