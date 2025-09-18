7d1ef8d3d1e21f254890067dbc0b04d1a15e451d
Dou Mok <douming.mok@gmail.com>
2025-09-18 12:13:55 -0700

    Add to 'V1.9' migration the 'populate_vb_code_identifiers'

--- helm/db/migrations/V1.9__populate_vb_identifiers_procedure.sql
+++ helm/db/migrations/V1.9__populate_vb_identifiers_procedure.sql
@@ -33,3 +33,33 @@ INSERT INTO vb_code_source_map (source_table, table_code, pk_column, id_type) VA
   ('taxonInterpretation', 'ti', 'taxoninterpretation_id', 'vb_code')
 ON CONFLICT DO NOTHING;
 
+-- Create procedure to insert new vb_codes for the tables above
+CREATE OR REPLACE PROCEDURE populate_vb_code_identifiers()
+LANGUAGE plpgsql -- PL/pgSQL is used for dynamic or iterative logic, rather than a static query
+AS $$ -- Begin body of the procedure
+DECLARE -- Declare variables
+  r RECORD; -- Holds one row at a time from the 'identifier_source_map'
+  sql TEXT; -- Holds the dynamically built SQL string
+BEGIN
+  FOR r IN SELECT * FROM vb_code_source_map LOOP
+    -- Use Postgres' format() function to safely build a SQL statement
+    -- Dollar-quoting (\$f\$ ... \$f\$) makes it easier to write multi-line SQL
+    -- without escaping single quotes; \$f\$ could also be written as \$\$
+    sql := format($f$
+      -- := assigns the string result from format() to the variable sql
+      INSERT INTO identifiers (vb_table_code, vb_record_id, identifier_type, identifier_value)
+      SELECT
+        %L,               -- table code string: e.g., 'pl'
+        %I,               -- PK column name: e.g., plot_id
+        %L,               -- identifier type: e.g., 'accessioncode'
+        %L || '.' || %I   -- build identifier_value: e.g. 'pl' || '.' || plot_id
+      FROM %s             -- table name: e.g., plot
+      WHERE accessionCode IS NOT NULL
+      ON CONFLICT (identifier_type, identifier_value) DO NOTHING;
+    $f$, r.table_code, r.pk_column, r.id_type,
+         r.table_code, r.pk_column, r.source_table);
+    EXECUTE sql; -- Run the dynamically built query
+  END LOOP;
+END$$;
+
+CALL populate_vb_code_identifiers();
