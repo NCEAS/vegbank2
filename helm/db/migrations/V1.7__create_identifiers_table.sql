c30a8e7593e4fb8d099acb1774825ae29652e7e9
Dou Mok <douming.mok@gmail.com>
2025-09-23 15:13:29 -0700

    Refactor V1.7 migration for clarity, moving the identifier_source_map creation to V1.8 migration where it is actually used in the procedure

--- helm/db/migrations/V1.7__create_identifiers_table.sql
+++ helm/db/migrations/V1.7__create_identifiers_table.sql
@@ -10,35 +10,3 @@ CREATE TABLE identifiers (
   identifier_value TEXT NOT NULL, -- e.g., "vb.ob.1234.abcd", "10.1234/abcd", "0000-0002-1825-0097"
   UNIQUE (identifier_type, identifier_value) -- Prevent duplicate mappings
 );
-
-
--- Creates a mapping table to iterate over for an the identifier import procedure
-CREATE TABLE identifier_source_map (
-  source_table TEXT NOT NULL,   -- table name
-  table_code   TEXT NOT NULL,   -- short code for the table
-  pk_column    TEXT NOT NULL,   -- primary key column of the source table
-  id_type      TEXT NOT NULL    -- identifier type (e.g., AccessionCode)
-);
-
--- Manually insert the mapping data
-INSERT INTO identifier_source_map (source_table, table_code, pk_column, id_type) VALUES
-  ('aux_role', 'ar', 'role_id', 'accession_code'),
-  ('commClass', 'cl', 'commclass_id', 'accession_code'),
-  ('commConcept', 'cc', 'commconcept_id', 'accession_code'),
-  ('commStatus', 'cs', 'commstatus_id', 'accession_code'),
-  ('coverMethod', 'cm', 'covermethod_id', 'accession_code'),
-  ('namedPlace', 'np', 'namedplace_id', 'accession_code'),
-  ('observation', 'ob', 'observation_id', 'accession_code'),
-  ('party', 'py', 'party_id', 'accession_code'),
-  ('plantConcept', 'pc', 'plantconcept_id', 'accession_code'),
-  ('plantStatus', 'ps', 'plantstatus_id', 'accession_code'),
-  ('plot', 'pl', 'plot_id', 'accession_code'),
-  ('project', 'pj', 'project_id', 'accession_code'),
-  ('reference', 'rf', 'reference_id', 'accession_code'),
-  ('referenceJournal', 'rj', 'referencejournal_id', 'accession_code'),
-  ('referenceParty', 'rp', 'referenceparty_id', 'accession_code'),
-  ('soilTaxon', 'st', 'soiltaxon_id', 'accession_code'),
-  ('stratumMethod', 'sm', 'stratummethod_id', 'accession_code'),
-  ('taxonObservation', 'to', 'taxonobservation_id', 'accession_code'),
-  ('taxonInterpretation', 'ti', 'taxoninterpretation_id', 'accession_code'),
-  ('userDefined', 'ud', 'userdefined_id', 'accession_code');
--- helm/db/migrations/V1.8__populate_identifiers_procedure.sql
+++ helm/db/migrations/V1.8__populate_identifiers_procedure.sql
@@ -1,6 +1,38 @@
 ----------------------------------------------------------------------------
--- Creates a procedure and then populate the identifiers table dynamically
+-- Creates a procedure to populate the identifiers table dynamically
+-- with the 'accession_code' identifier_type
 ----------------------------------------------------------------------------
+
+-- Create a mapping table to iterate over for an the identifier import procedure
+CREATE TABLE identifier_source_map (
+  source_table TEXT NOT NULL,   -- table name
+  table_code   TEXT NOT NULL,   -- short code for the table
+  pk_column    TEXT NOT NULL,   -- primary key column of the source table
+  id_type      TEXT NOT NULL    -- identifier type (e.g., AccessionCode)
+);
+
+-- Manually insert the mapping data
+INSERT INTO identifier_source_map (source_table, table_code, pk_column, id_type) VALUES
+  ('aux_role', 'ar', 'role_id', 'accession_code'),
+  ('commClass', 'cl', 'commclass_id', 'accession_code'),
+  ('commConcept', 'cc', 'commconcept_id', 'accession_code'),
+  ('commStatus', 'cs', 'commstatus_id', 'accession_code'),
+  ('coverMethod', 'cm', 'covermethod_id', 'accession_code'),
+  ('namedPlace', 'np', 'namedplace_id', 'accession_code'),
+  ('observation', 'ob', 'observation_id', 'accession_code'),
+  ('party', 'py', 'party_id', 'accession_code'),
+  ('plantConcept', 'pc', 'plantconcept_id', 'accession_code'),
+  ('plantStatus', 'ps', 'plantstatus_id', 'accession_code'),
+  ('plot', 'pl', 'plot_id', 'accession_code'),
+  ('project', 'pj', 'project_id', 'accession_code'),
+  ('reference', 'rf', 'reference_id', 'accession_code'),
+  ('referenceJournal', 'rj', 'referencejournal_id', 'accession_code'),
+  ('referenceParty', 'rp', 'referenceparty_id', 'accession_code'),
+  ('soilTaxon', 'st', 'soiltaxon_id', 'accession_code'),
+  ('stratumMethod', 'sm', 'stratummethod_id', 'accession_code'),
+  ('taxonObservation', 'to', 'taxonobservation_id', 'accession_code'),
+  ('taxonInterpretation', 'ti', 'taxoninterpretation_id', 'accession_code'),
+  ('userDefined', 'ud', 'userdefined_id', 'accession_code');
 
 CREATE OR REPLACE PROCEDURE populate_acc_code_identifiers()
 LANGUAGE plpgsql -- PL/pgSQL is used for dynamic or iterative logic, rather than a static query
