0a57cb912353b0c3999ab6351b9f1e4194f40433
Jim Regetz <regetz@nceas.ucsb.edu>
2025-09-25 22:33:15 -0700

    Add new PlantConcept operator and associated SQL queries
    This operator:
    - Currently only implements a GET endpoint (i.e., data retrieval)
    - Follows the existing pattern around implementing both single record
      and paginated collection retrieval
    - Deviates from other existing endpoints by retrieving Plant Concepts by
      their *vb_code* (per new design) rather than by accession code.
    - Leverages new base Operator methods to return data as either JSON or
      Parquet, currently controlled by a GET query parameter
    - Deviates from other existing endpoints by storing DB configuration
      parameters as an operator attribute rather than explicitly passing
      them via the data getter method
    - Introduces a new method for doing request parameter validation,
      raising a custom QueryParameterError exception if validation fails

--- vegbank/operators/__init__.py
+++ vegbank/operators/__init__.py
@@ -4,6 +4,7 @@ from .CommunityClassification import CommunityClassification
 from .CommunityConcept import CommunityConcept
 from .CoverMethod import CoverMethod
 from .Party import Party
+from .PlantConcept import PlantConcept
 from .PlotObservation import PlotObservation
 from .Project import Project
 from .StratumMethod import StratumMethod
