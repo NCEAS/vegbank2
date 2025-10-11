e3bb176d62de82d2eb352790fdbd871cb3c9bdaa
Jim Regetz <regetz@nceas.ucsb.edu>
2025-10-11 08:53:25 -0700

    Align Community Concept endpoints with Plant Concepts

--- queries/community_concept/get_community_concept_count.sql
+++ queries/community_concept/get_community_concept_count.sql
@@ -1,6 +1,2 @@
-SELECT
-    count(*)
-FROM
-    commConcept
-    left join commname on commconcept.commname_id = commname.commname_id
-    left join reference on commName.reference_id = reference.reference_id;
+SELECT COUNT(*)
+  FROM commconcept
