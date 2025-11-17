23475d1d6ee87cd5e71e498c3abeb3fe309a901b
RWShelton <rshelton@nceas.ucsb.edu>
2025-11-17 10:36:33 -0800

    Adding comment describing input ordering in table_defs_config.py

--- queries/taxon_observation/taxon_importance/insert_taxon_importance_to_temp_table.sql
+++ queries/taxon_observation/taxon_importance/insert_taxon_importance_to_temp_table.sql
@@ -1,3 +1,4 @@
+-- Ordering of input parameters is handled via list order in table_defs_config.py
 INSERT INTO taxon_importance_temp
     (
         user_tm_code,
--- queries/taxon_observation/taxon_observation/insert_taxon_observation_to_temp_table.sql
+++ queries/taxon_observation/taxon_observation/insert_taxon_observation_to_temp_table.sql
@@ -1,3 +1,4 @@
+-- Ordering of input parameters is handled via list order in table_defs_config.py
 INSERT INTO taxon_observation_temp
     (
         user_to_code,
