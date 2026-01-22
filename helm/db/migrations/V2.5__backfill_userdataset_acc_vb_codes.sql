7557f00f7ae2314d78359ded1aafbfd883a4b499
Dou Mok <douming.mok@gmail.com>
2026-01-22 12:55:10 -0800

    Remove redundant and conflicting 'BEGIN' and 'COMMIT' sql statements in V2.5 migration

--- helm/db/migrations/V2.5__backfill_userdataset_acc_vb_codes.sql
+++ helm/db/migrations/V2.5__backfill_userdataset_acc_vb_codes.sql
@@ -3,17 +3,13 @@
 -- re-execute procedures to populate 'identifiers' table
 ----------------------------------------------------------------------------
 
-BEGIN;
-
 -- Add missing mapping rows
 INSERT INTO identifiers_source_map (source_table, table_code, pk_column, id_type)
 VALUES
-  ('userdataset', 'ds', 'userdataset_id', 'accession_code'),
-  ('userdataset', 'ds', 'userdataset_id', 'vb_code')
+  ('userdataset', 'ud', 'userdataset_id', 'accession_code'),
+  ('userdataset', 'ud', 'userdataset_id', 'vb_code')
 ON CONFLICT DO NOTHING;
 
 -- Call procedures (which can handle conflicts)
 CALL populate_acc_code_identifiers();
 CALL populate_vb_code_identifiers();
-
-COMMIT;
