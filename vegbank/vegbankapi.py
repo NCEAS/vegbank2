475d16ec48a723406a0eebf6f23929a98118e973
Dou Mok <douming.mok@gmail.com>
2026-02-03 14:36:14 -0800

    Refactor 'identifiers' function exception handling

--- vegbank/vegbankapi.py
+++ vegbank/vegbankapi.py
@@ -1058,9 +1058,15 @@ def identifiers(identifier_value):
                         supported = row["vb_table_code"] in {"od", "cc", "ds"}
                         row["supported_redirect"] = supported
                         return jsonify(row), 200
-        except Exception:
+        # pylint: disable=W0718
+        except Exception as e:
             print(traceback.format_exc())
-            raise
+            return (
+                jsonify_error_message(
+                    f"Unexpected error during identifier search: {str(e)}"
+                ),
+                500,
+            )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80,debug=True)
