ab810015c9668a664afce969532417490e630fb5
Dou Mok <douming.mok@gmail.com>
2026-02-06 10:18:59 -0800

    Add pytests for importing vegbank modules utilities, operators and repositories

--- tests/test_vegbank2_app.py
+++ tests/test_vegbank2_app.py
@@ -1,4 +1,13 @@
 # Overall application tests that aren't specific to a particular module
 
-def test_always_passes():
-    assert True
+def test_import_utilties():
+    """COnfirm that we can import the utilities module from vegbank"""
+    import vegbank.utilities
+
+def test_import_repositories():
+    """Confirm that we can import the repositories module from vegbank"""
+    import vegbank.repositories
+
+def test_import_operators():
+    """COnfirm that we can import the operators module from vegbank"""
+    import vegbank.operators
