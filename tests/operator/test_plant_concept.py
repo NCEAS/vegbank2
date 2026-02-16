5f0b201a25e41f21a5aba4b7c3ce5b492445eaa0
Dou Mok <douming.mok@gmail.com>
2026-02-16 14:05:11 -0800

    Add initial 'plant_concept' test module with basic pytest and queries package test for 'upload_plant_names' function

---
+++ tests/operator/test_plant_concept.py
@@ -0,0 +1,59 @@
+"""Test module for the 'CommunityConcept' operator class"""
+from unittest.mock import MagicMock, patch
+import pandas as pd
+from vegbank.operators import (
+    Operator,
+    PlantConcept,
+)
+
+params = {}
+
+def test_queries_package_attribute():
+    """Check that the queries package attribute was overridden as expected."""
+    plant_concept_operator = PlantConcept(params)
+    assert (
+        plant_concept_operator.queries_package
+        == "vegbank.queries.plant_concept"
+    )
+
+
+def test_queries_package_upload_plant_names():
+    """Check that the queries package attribute was overridden as expected when calling
+    the 'upload_plant_names' function."""
+    plant_concept_operator = PlantConcept(params)
+    # Create a basic dataframe with the required keys
+    df = pd.DataFrame([
+        {
+            "user_pc_code": "VB.PC.DM.001",
+            "vb_pc_code": "test.dm.pc.1",
+            "name": "dou.test",
+            "name_type": "dou.string",
+            "name_status": "name.status",
+        }
+    ])
+    # This connection represents access to a db
+    conn = MagicMock()
+
+    # Mock return values for 'upload_to_table'
+    pn_actions = {
+        "resources": {
+            "pn": {
+                "user_pn_code": ["dou.test"],
+                "vb_pn_code": ["VB.CN.001"],
+            }
+        },
+        "counts": {"pn": 1},
+    }
+    pu_actions = {
+        "resources": {"pu": []},
+        "counts": {"pn": 0, "pu": {}},
+    }
+
+    # Side effect allows the initial call to return the first object,
+    # then the subsequent to call the second object
+    with patch.object(
+        Operator, "upload_to_table", side_effect=[pn_actions, pu_actions]
+    ) as _mocked:
+        plant_concept_operator.upload_plant_names(df, conn)
+
+    assert plant_concept_operator.queries_package == "vegbank.queries.plant_concept.plant_name"
