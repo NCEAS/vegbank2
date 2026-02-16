363f0f3fb90890ba815498ee295a1e445be1ccd5
Dou Mok <douming.mok@gmail.com>
2026-02-16 12:43:18 -0800

    Add 'party' test module with basic test

---
+++ tests/operator/test_party.py
@@ -0,0 +1,14 @@
+"""Test module for the 'CommunityConcept' operator class"""
+from vegbank.operators import (
+    Party,
+)
+
+params = {}
+
+def test_queries_package_attribute():
+    """Check that the queries package attribute was overridden as expected."""
+    cover_method_operator = Party(params)
+    assert (
+        cover_method_operator.queries_package
+        == "vegbank.queries.party"
+    )
