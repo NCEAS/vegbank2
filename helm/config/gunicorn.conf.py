8713cb9442f5e8f4947c4e68e6e094be302fddb8
Matthew B <106352182+artntek@users.noreply.github.com>
2026-03-19 13:25:14 -0700

    move configurable settings to values

--- helm/config/gunicorn.conf.py
+++ helm/config/gunicorn.conf.py
@@ -1,11 +1,11 @@
 import os
 
-workers = os.getenv('GUNICORN_WORKERS', default=5)
+workers = os.getenv('GUNICORN_WORKERS', default={{ .Values.gunicorn.workers }})
 
-timeout = os.getenv('GUNICORN_TIMEOUT', default=2400)
+timeout = os.getenv('GUNICORN_TIMEOUT', default={{ .Values.gunicorn.timeout }})
 
-bind = os.getenv('GUNICORN_PORT', default="0.0.0.0:8000") 
+bind = os.getenv('GUNICORN_PORT', default="0.0.0.0:{{ .Values.service.port }}")
 
 accesslog = os.getenv('GUNICORN_ACCESSLOG', default='-') # Access Log to stdout
 errorlog = os.getenv('GUNICORN_ERRORLOG', default='-') # Error Log to stdout
-loglevel = os.getenv('GUNICORN_LOGLEVEL', default='debug')
+loglevel = os.getenv('GUNICORN_LOGLEVEL', default='{{ .Values.gunicorn.logLevel }}')
