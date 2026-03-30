7fc51caa064937d67cc5ee9e7083c0770befe4d3
RWShelton <rshelton@nceas.ucsb.edu>
2026-03-30 16:53:45 -0700

    Swapping log level set to create_app so gunicorn picks it up, adding dev marker to local log message.
    Still to do: pull log level from config file

--- src/vegbank/app.py
+++ src/vegbank/app.py
@@ -8,11 +8,11 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(main)
     app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', os.urandom(32).hex())
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
     init_oauth(app)
     return app
 
 if __name__ == '__main__':
     app = create_app()
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-    logging.info("Starting VegBank API server...")
+    logging.info("Starting DEV VegBank API server...")
     app.run(port=8000)
